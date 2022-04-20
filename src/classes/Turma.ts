a1dfb7b7fe0586b152d5a2348a7f8dff10f2d005
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-04-20 16:14:55 -0300

    subindo setup do projeto

---
+++ src/classes/Turma.ts
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+export class Turma {
+    id: string
+    nome: string
+    modulo: string
+    constructor(nome:string, modulo:string) {
+        this.id = uuidv4 ()
+        this.nome = nome
+        this.modulo = modulo
+    }
+    public retornaTurma(): string{
+        return this.nome
+     }
+}
