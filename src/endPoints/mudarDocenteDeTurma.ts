e1b48bc16271427096eaa7a219c36689501317a3
Ana <a.sammi1103@gmail.com>
2022-04-22 16:20:52 -0300

    mudar turma arrumado

--- src/endPoints/mudarDocenteDeTurma.ts
+++ src/endPoints/mudarDocenteDeTurma.ts
@@ -4,16 +4,19 @@ import { connection } from '../data/connections';
 export const mudarTurmaDocente = async(req: Request, res: Response) => {
     const {turma_id, docente_id} = req.body
     try {
-        let resultado = await connection("estudante")
-            .update({
-                turma_id: turma_id
-            })
-            .where("id", docente_id)
-            
+        console.log(turma_id, docente_id)
         if(!turma_id || !docente_id) {
             throw new Error("Preencha todos os campos")
         }
-        res.status(200).send(resultado)
+        await connection("docente")
+            .update({
+                turma_id: turma_id
+            })
+            .where({
+                id: docente_id
+            })
+            
+        res.status(200).send("O docente mudou de turma")
     }
     catch(e: any) {
         res.status(500).send(e.sqlMessage || e.message)
