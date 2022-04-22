eb104c4e805403ebb697182f6bf594eec7789b41
Ana <a.sammi1103@gmail.com>
2022-04-22 09:39:26 -0300

    alteração no migrations

--- src/data/migrations.ts
+++ src/data/migrations.ts
@@ -11,51 +11,51 @@ const printError = (error: any) => { console.log(error.sqlMessage || error.messa
 const createTable =() => connection.raw(`
     CREATE TABLE IF NOT EXISTS turma(
         id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL,
+        nome VARCHAR(255) UNIQUE NOT NULL,
         modulo VARCHAR(255) DEFAULT 0
     );
 
     CREATE TABLE IF NOT EXISTS estudante(
         id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE
+        nome VARCHAR(255) UNIQUE NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
         data_nasc DATE NOT NULL,
-        turma_id VARCHAR(255) NOT NULL
+        turma_id VARCHAR(255) NOT NULL,
         FOREIGN KEY (turma_id) REFERENCES turma(id)
     );
 
     CREATE TABLE IF NOT EXISTS docente(
         id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE
+        nome VARCHAR(255) UNIQUE NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
         data_nasc DATE NOT NULL,
-        turma_id VARCHAR(255) NOT NULL
+        turma_id VARCHAR(255) NOT NULL,
         FOREIGN KEY (turma_id) REFERENCES turma(id)
     );
 
     CREATE TABLE IF NOT EXISTS hobby(
         id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL
+        nome VARCHAR(255) UNIQUE NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS especialidade(
         id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL
+        nome VARCHAR(255) UNIQUE NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS docente_especialidade(
         id VARCHAR(255) PRIMARY KEY,
-        docente_id VARCHAR(255) NOT NULL
-        especialidade_id VARCHAR(255) NOT NULL
-        FOREIGN KEY (docente_id) REFERENCES docente(id)
+        docente_id VARCHAR(255) NOT NULL,
+        especialidade_id VARCHAR(255) NOT NULL,
+        FOREIGN KEY (docente_id) REFERENCES docente(id),
         FOREIGN KEY (especialidade_id) REFERENCES especialidade(id)
     );
 
     CREATE TABLE IF NOT EXISTS estudante_hobby(
         id VARCHAR(255) PRIMARY KEY,
-        estudante_id VARCHAR(255) NOT NULL
-        hobby_id VARCHAR(255) NOT NULL
-        FOREIGN KEY (estudante_id) REFERENCES docente(id)
+        estudante_id VARCHAR(255) NOT NULL,
+        hobby_id VARCHAR(255) NOT NULL,
+        FOREIGN KEY (estudante_id) REFERENCES docente(id),
         FOREIGN KEY (hobby_id) REFERENCES especialidade(id)
     );
 
